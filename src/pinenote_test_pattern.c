c10036ffc462dc7035e982f4904c4798b11dc4b5
Visti Andresen <talpa@talpa.dk>
2023-11-22 14:05:25 +0100

    Draws the colour bands now

--- src/pinenote_test_pattern.c
+++ src/pinenote_test_pattern.c
@@ -3,7 +3,8 @@
 //#cflags -I/usr/include/SDL2
 //#ldflags -lSDL2
 
-#include  <SDL.h>
+#include <SDL.h>
+#include <stdio.h>
 
 
 SDL_Renderer *renderer_;
@@ -12,6 +13,14 @@ SDL_Window *window_;
 #define WIDTH (1872)
 #define HEIGHT (1404)
 
+static uint8_t keepRunning_ = 1;
+static uint8_t needsARedraw_ = 1;
+static uint8_t drawDirection_ = 0;
+
+static int rectWidth_ = 42;
+static int rectHeight_ = 42;
+
+SDL_Surface *colours_[16];
 
 void initSDL(void) {
   int rendererFlags, windowFlags;
@@ -39,9 +48,81 @@ void initSDL(void) {
     printf("Failed to create renderer: %s\n", SDL_GetError());
     exit(1);
   }
+
+  SDL_Rect fillRect;
+  fillRect.x = 0;
+  fillRect.y = 0;
+  fillRect.w = rectWidth_;
+  fillRect.h = rectHeight_;
+  for (Uint32 i = 0; i < 16; i++) {
+    colours_[i] = SDL_CreateRGBSurface(0, rectWidth_, rectHeight_, 24, 0, 0, 0, 0);
+    Uint32 colour = (i << (16 + 4)) | (i << (8 + 4)) | (i << (0 + 4));
+    SDL_FillRect(colours_[i], &fillRect, colour);
+  }
+}
+
+static void checkInput(void) {
+  SDL_Event event;
+  while (SDL_PollEvent(&event)) {
+    switch (event.type) {
+    case SDL_QUIT:
+      keepRunning_ = 0;
+      break;
+    case SDL_FINGERDOWN:{
+	float x = event.tfinger.x;
+	float y = event.tfinger.y;
+	printf("finger down @ %f,%f\n", x,y);
+	drawDirection_ ^= 1;
+	needsARedraw_ = 1;
+      }
+      break;
+      case SDL_MOUSEBUTTONDOWN: {
+	Sint32 x = event.button.x;
+	Sint32 y = event.button.y;
+	printf("mouse down @ %d,%d\n", x,y);
+	drawDirection_ ^= 1;
+	needsARedraw_ = 1;
+      }
+    }
+  }
+}
+
+static void drawScreen(void) {
+  SDL_Rect sourceRect;
+  sourceRect.x = 0;
+  sourceRect.y = 0;
+  sourceRect.w = rectWidth_;
+  sourceRect.h = rectHeight_;
+  SDL_Rect destinationRect;
+  destinationRect.w = rectWidth_;
+  destinationRect.h = rectHeight_;
+  SDL_Surface *windowSurface = SDL_GetWindowSurface(window_);
+  for (int x=0; x<16; x++){
+    for (int y = 0; y < 16; y++) {
+        destinationRect.x = x * rectWidth_;
+        destinationRect.y = y * rectHeight_;
+        int colour;
+        if (drawDirection_) {
+          colour = y;
+        }
+	else {
+          colour = x;
+        }
+
+        SDL_BlitSurface(colours_[colour], &sourceRect, windowSurface, &destinationRect);
+    }
+  }
+  SDL_UpdateWindowSurface(window_);
+  needsARedraw_ = 0;
 }
 
 int main(int argc, char **args) {
   initSDL();
+  while (keepRunning_) {
+    checkInput();
+    if (needsARedraw_) {
+      drawScreen();
+    }
+  }
   return 0;
 }
